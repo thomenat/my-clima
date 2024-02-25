1623750e33254760300289412fb48e30c025109e
thomenat <156375450+thomenat@users.noreply.github.com>
2024-02-25 18:44:20 +0000

    updated js file functions to update upon search of new city

--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
 function refreshWeather(response) {
-    console.log(response.data);
+  let temperatureElement = document.querySelector("#temperature");
+  let temperature = response.data.temperature.current;
+  let cityElement = document.querySelector("#city");
+
+  cityElement.innerHTML = response.data.city;
+  temperatureElement.innerHTML = Math.round(temperature);
+  
 }
 
 
@@ -16,11 +22,12 @@ function searchCity(city) {
 function handleSearchSubmit(event) {
     event.preventDefault();
     let searchInput = document.querySelector("#search-form-input");
-    let cityElement = document.querySelector("#city");
-    cityElement.innerHTML = searchInput.value;
+   
+    searchCity(searchInput.value);
 }
 
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
 
+searchCity("Goiânia");
