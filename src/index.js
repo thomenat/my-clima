1ee74689104c7d8808f8eff14b18dabd1058ee19
thomenat <156375450+thomenat@users.noreply.github.com>
2024-02-25 17:55:23 +0000

    added functions to refresh city data

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
+function refreshWeather(response) {
+    console.log(response.data);
+}
+
+
+function searchCity(city) {
+    let apiKey = "deabbt600bd7ofd44dbd308802faa2f2";
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}}&key=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(refreshWeather);
+    
+}
+
+
+
 
 function handleSearchSubmit(event) {
     event.preventDefault();
